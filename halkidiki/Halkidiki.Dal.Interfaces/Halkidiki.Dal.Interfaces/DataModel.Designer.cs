385f450345063fd817a973792f44b3c8b151ac00
Michał Wojciechowski <michal.wojciechowski@outlook.com>
2019-03-29 08:05:42 +0100

    po poprawce

--- halkidiki/Halkidiki.Dal.Interfaces/Halkidiki.Dal.Interfaces/DataModel.Designer.cs
+++ halkidiki/Halkidiki.Dal.Interfaces/Halkidiki.Dal.Interfaces/DataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Administrator\Documents\toci_newhope\halkidiki\Halkidiki.Dal.Interfaces\Halkidiki.Dal.Interfaces\DataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\micha\OneDrive\Dokumenty\toci_newhope1\halkidiki\Halkidiki.Dal.Interfaces\Halkidiki.Dal.Interfaces\DataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
